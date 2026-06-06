91b138659d67710f4ede2883efab18fa8179ce34
Ropan Datta <83666754+ropan1@users.noreply.github.com>
2026-06-06 19:13:06 +0530

    chore: initialize Flutter project scaffold and gitignore
    Generated Flutter project scaffold for android, ios, linux, macos, windows, and web targets, plus pubspec, analysis options, and gitignore.

---
+++ android/app/src/main/kotlin/com/example/verovio_webview_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.verovio_webview_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_picker
+import record_macos
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
